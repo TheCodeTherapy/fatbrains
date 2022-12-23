1b0e1af957b0c3f2d28d18f276b8b10e858ca8bb
TheCodeTherapy <me@mgz.me>
2022-12-23 21:40:26 +0000

    adds rg to extra installs script because I always forget to install it

--- z_extras.sh
+++ z_extras.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 if [[ "$OSTYPE" == "darwin"* ]]; then
   npm install -g typescript-language-server typescript @fsouza/prettierd eslint_d
+  brew install ripgrep
 elif if [[ "$OSTYPE" == "linux-gnu"* ]]; then
   npm install -g neovim typescript-language-server typescript \
     tree-sitter tree-sitter-cli @fsouza/prettierd eslint_d
