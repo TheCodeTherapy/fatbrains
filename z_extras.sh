f2491b2f1f5e093ddaf491a5ffd84626b2cfed85
TheCodeTherapy <me@mgz.me>
2022-12-23 19:41:21 +0000

    updates extra installs script to MacOS

--- z_extras.sh
+++ z_extras.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-npm install -g neovim typescript-language-server typescript tree-sitter tree-sitter-cli @fsouza/prettierd eslint_d
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  npm install -g typescript-language-server typescript @fsouza/prettierd eslint_d
+elif if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+  npm install -g neovim typescript-language-server typescript \
+    tree-sitter tree-sitter-cli @fsouza/prettierd eslint_d
+fi
 
