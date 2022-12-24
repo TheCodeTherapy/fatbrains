ef4672c3647adeab9cd95b3cfc0d5c3c7f1caf68
TheCodeTherapy <me@mgz.me>
2022-12-24 10:52:07 +0000

    updates extra installs script

--- z_extras.sh
+++ z_extras.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
+
 if [[ "$OSTYPE" == "darwin"* ]]; then
-  npm install -g typescript-language-server typescript @fsouza/prettierd eslint_d
+  npm install -g typescript-language-server typescript \
+    @fsouza/prettierd eslint_d yarn
+  brew install stylua
+  brew install markdownlint-cli
   brew install ripgrep
-elif if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
   npm install -g neovim typescript-language-server typescript \
     tree-sitter tree-sitter-cli @fsouza/prettierd eslint_d
 fi
-
